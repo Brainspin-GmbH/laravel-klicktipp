1ae678afa40d0faf48be172688195f203f72be07
David Friedrich <david@brainspin.de>
2021-06-16 17:45:40 +0200

    Add test subcriber to config

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'service'       => 'https://api.klicktipp.com',
-    'username'      => env('KLICKTIPP_USERNAME', null),
-    'password'      => env('KLICKTIPP_PASSWORD', null),
-    'developer_key' => env('KLICKTIPP_DEVELOPER_KEY', null),
-    'customer_key'  => env('KLICKTIPP_CUSTOMER_KEY', null),
+    'service'         => 'https://api.klicktipp.com',
+    'username'        => env('KLICKTIPP_USERNAME', null),
+    'password'        => env('KLICKTIPP_PASSWORD', null),
+    'developer_key'   => env('KLICKTIPP_DEVELOPER_KEY', null),
+    'customer_key'    => env('KLICKTIPP_CUSTOMER_KEY', null),
+    'test_subscriber' => env('KLICKTIPP_TEST_SUBSCRIBER', null),
 ];
